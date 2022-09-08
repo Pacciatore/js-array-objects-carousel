7193ed50cd401d9658622540da21a512a9caf75c
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-08 02:39:42 +0200

    Uso array di oggetti

--- js/script.js
+++ js/script.js
@@ -14,7 +14,36 @@ const NUM_IMAGES = 5;
 const CHANGE_IMAGE_DELAY = 2;
 
 
-const images = createImageArray(NUM_IMAGES);
+// Array di immagini con url web
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
 console.log(images);
 
 let activeIndex = 0;
@@ -38,7 +67,7 @@ rightButton.addEventListener('click', nextCarouselImage);
 
 
 // Functions
-
+// Obsoleta
 function createImageArray(numImages) {
 
     const images = [];
@@ -54,14 +83,14 @@ function createImageArray(numImages) {
 }
 
 
-function buildCarousel(urls, activeIndex) {
+function buildCarousel(images, activeIndex) {
 
     const carouselImages = document.querySelector('.carousel-images');
     const carouselThumbs = document.querySelector('.carousel-thumbs');
     let content = '';
 
-    for (let i = 0; i < urls.length; i++) {
-        const url = urls[i];
+    for (let i = 0; i < images.length; i++) {
+        const url = images[i].url;
         const imageClass = i === activeIndex ? 'tr-carousel-img active' : 'tr-carousel-img';
         // Composizione elemento html img
         content += `<img class="${imageClass}" src="${url}" alt="${url}" />`
